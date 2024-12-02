ff8d82f6808014a98fff81394b8b16db1472f5e1
thanpisit-film-hub <tanpisit.2548@gmail.com>
2024-12-02 19:14:51 +0700

    Filled out README

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,36 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int i = 0, k = 0, l = 0, z = 0;
+
+int myFC1(int x){
+    if(z%2 == 0 and z != 0){
+        k += 1;
+    }
+    if(z%2 != 0 and z != 0){
+        l += 1;
+    }
     return 0;
 }
+
+int main(){
+
+    cout << "Enter an integer: ";
+    cin >> z;
+
+    myFC1(z);
+
+    while(z != 0){
+        cout << "Enter an integer: ";
+        cin >> z;
+        myFC1(z);
+        if(z == 0){
+            break;
+        }
+    }
+
+    cout << "#Even numbers = " << k << endl;
+    cout << "#Odd numbers = " << l;
+    return 0;
+}
+
--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double x){
+
+    x = (x*M_PI)/180;
+    return x;
+}
+
+double rad2deg(double y){
+    y = (y*180)/M_PI;
+    return y;
+}
+
+double findXComponent(double l1,double l2,double a1, double a2){
+
+    double x_num1 = l1 * cos(a1);
+    double x_num2 = l2 * cos(a2);
+    double x_sum = x_num1 + x_num2;
+    return x_sum;
+
+}
+
+double findYComponent(double l1,double l2,double a1, double a2){
+
+    double y_num1 = l1 * sin(a1);
+    double y_num2 = l2 * sin(a2);
+    double y_sum = y_num1 + y_num2;
+    return y_sum;
+
+}
+
+double pythagoras(double xcomp, double ycomp){
+
+    double _sizeof = sqrt(xcomp*xcomp + ycomp*ycomp);
+    return _sizeof;
+}
+
+void showResult(double result_vec_length, double result_vec_direction){
+
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = " << result_vec_length << endl;
+    cout << "Direction of the resultant vector (deg) = " << result_vec_direction << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
